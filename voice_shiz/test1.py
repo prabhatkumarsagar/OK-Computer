6bddd84aaa6d024334e62798f6426f0bf6034f59
prabhatkumarsagar <sagarprabhatkumar13@gmail.com>
2020-10-14 16:46:58 +0530

    gg

--- voice_shiz/test1.py
+++ voice_shiz/test1.py
@@ -4,58 +4,48 @@ import speech_recognition as sr
 import datetime
 engine=pyttsx3.init()
 voices=engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)
-engine.setProperty('rate',125)
-engine.say('Hello there sire Prabhat how may i be of any fucking use today?')
-engine.runAndWait()
+engine.setProperty('voice',voices[10].id)
+engine.setProperty('rate',125) #Default Rate = 150
 
-"""
-def speak(audio): 
-    engine.say(audio) 
+def voice_out(qry): 
+    engine.say(qry) 
     engine.runAndWait()
 
-def wishMe(): 
-    hour = int(datetime.datetime.now().hour) 
-    if hour>= 0 and hour<12: 
-        speak("Good Morning Sir !") 
-   
-    elif hour>= 12 and hour<18: 
-        speak("Good Afternoon Sir !")    
-   
-    else: 
-        speak("Good Evening Sir !")   
-   
-    assname =("Saturday 1 point o") 
-    speak("I am your Personal Desktop Assistant") 
-    speak(assname)
-    usrname()
+def chng_voice_rate(newrate):
+    engine.setProperty('rate',newrate)
 
-def takeCommand():
+def chng_voice_lang(x):
+    engine.setProperty('voice',voices[x].id)
+
+"""
+VOICE IDS
+german 8
+default 10
+english 11
+english-us 16
+spanish 19
+french 26
+hindi 29
+malayalam 46
+punjabi 51
+russian 56
+tamil 62
+Mandarin 67
+"""
+
+
+def voice_in():
     r = sr.Recognizer() 
-    with sr.Microphone() as source: 
-        print("Listening...") 
-        r.pause_threshold = 1
+    with sr.Microphone() as source:  
+        print("speak now")
         audio = r.listen(source) 
    
-    try: 
-        print("Recognizing...")     
+    try:    
         query = r.recognize_google(audio, language ='en-in') 
-        print(f"User said: {query}\n") 
-   
-    except: 
-        print(e)     
-        print("Unable to Recognizing your voice.")   
-        return "None"
-      
-    return query
+        print("you said: ",query)
 
-def usrname(): 
-    speak("What should i call you sir") 
-    uname = takeCommand() 
-    speak("Welcome Mister") 
-    speak(uname) 
-    print("Welcome Mr f'{uname}")
-    speak("How can i Help you, Sir") 
+    except:   
+        print("Sorry i didn't get that! Please try again.")   
 
-wishMe()
-"""
+
+voice_in()
