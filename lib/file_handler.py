dcf020ea67affd62e96b6a0a5ade0f64f0194ee3
DuttaAB-dev <61903332+DuttaAB-dev@users.noreply.github.com>
2020-10-17 21:20:01 +0530

    ...

--- lib/file_handler.py
+++ lib/file_handler.py
@@ -1,8 +1,12 @@
 import os
 class File_Handler:
+    operation = []
 
     def __init__(self, operation):
-        operation = []
+        self.operation = operation
+
+    def fileSearch(self):
+        
     
     
-    
+    def deleteFile(self,):
