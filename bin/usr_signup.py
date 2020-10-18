565a9ad449493babfc08f4315f29ad969e02ce84
prabhatkumarsagar <sagarprabhatkumar13@gmail.com>
2020-10-18 10:07:37 +0530

    day 4 (mysql shit for user signup init)

--- bin/usr_signup.py
+++ bin/usr_signup.py
@@ -1,12 +1,20 @@
 #sign_up screen 1
 #asks the user to enter his/her mysql password 
 import mysql.connector as sql #pip install mysql-connector-python
-usr = input("Enter your MySQL user-name : ")
-try:
-    pwd = input("Enter your MySQL password: ")
-    c = sql.connect(host = "127.0.0.1", user = usr, passwd = pwd)
-except:
-    print("Uh oh! It looks like i could not connect to MySQL, please make sure MySQL is properly installed and you have entered the correct username and password.")
 
-cur = c.cursor()
-cur.execute("create database if not exists user_info_christmas_presents_for_nsa;")
+while True:
+    try:
+        usr = input("Enter your MySQL username('root' by default): ")
+        pwd = input("Enter your MySQL password: ")
+        if usr=='':
+            usr="root"
+        con = sql.connect(host = "127.0.0.1", user = usr, passwd = pwd)
+    except:
+        print("Uh oh! It looks like i could not connect to MySQL, please make sure MySQL is properly installed and you have entered the correct username and password.")
+        break
+cur = con.cursor()
+cur.execute("create database if not exists pydeskassist;")
+cur.execute("use pydeskassist;")
+cur.execute("create table if not exists usr_info(S_No int(1), Name varchar(100), DOB date, Gender(M/F) char(1), Email varchar(100), Password varchar(30));
+
+#sign_up screen 2
