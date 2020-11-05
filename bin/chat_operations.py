0d5d812c94ad92cd4cd36a943d0af47efa6ae822
prabhatkumarsagar <sagarprabhatkumar13@gmail.com>
2020-11-05 18:52:47 +0530

    shiz casddsada

--- bin/chat_operations.py
+++ bin/chat_operations.py
@@ -18,22 +18,21 @@ joke()
 
 #help
 def help():
-    pass
+    """
+    You can use these commands and I'll help you out:
+    1. Open reddit subreddit : Opens the subreddit in default browser.
+    2. Open xyz.com : replace xyz with any website name
+    3. Send email/email : Follow up questions such as recipient name, content will be asked in order.
+    4. Current weather in {cityname} : Tells you the current condition and temperture
+    5. Hello
+    6. play me a video : Plays song in your VLC media player
+    7. change wallpaper : Change desktop wallpaper
+    8. news for today : reads top news of today
+    9. time : Current system time
+    10. top stories from google news (RSS feeds)
+    11. tell me about xyz : tells you about xyz
+    """
 
 #greet
 def greet():
     pass
-elif 'help me' in command:
-        sofiaResponse("""
-        You can use these commands and I'll help you out:1. Open reddit subreddit : Opens the subreddit in default browser.
-        2. Open xyz.com : replace xyz with any website name
-        3. Send email/email : Follow up questions such as recipient name, content will be asked in order.
-        4. Current weather in {cityname} : Tells you the current condition and temperture
-        5. Hello
-        6. play me a video : Plays song in your VLC media player
-        7. change wallpaper : Change desktop wallpaper
-        8. news for today : reads top news of today
-        9. time : Current system time
-        10. top stories from google news (RSS feeds)
-        11. tell me about xyz : tells you about xyz
-        """)
